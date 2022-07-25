a53f0fa3a18937f867ecfdea33c6cf2245f597fa
Samuel Costa Ferreira <106633009+samuhcosta@users.noreply.github.com>
2022-07-25 17:07:54 -0300

    Tratamento de erros - Exception

--- src/main/java/com/example/algamoney/api/esceptionHandler/AlgamoneyExceptionHandler.java
+++ src/main/java/com/example/algamoney/api/esceptionHandler/AlgamoneyExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.example.algamoney.api.esceptionHandler;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,13 +11,42 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+//esta de olho na aplicacao, e controla as mensagens dos erros
 @ControllerAdvice
 public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler{
 	
+	@Autowired
+	private MessageSource messageSource;
+	
+	//capturando uma mensagem específica de erro que pode ter
 	@Override
 	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
 		
-		return handleExceptionInternal(ex, "Mensagem inválida!", headers, HttpStatus.BAD_REQUEST, request);
+		String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
+		String mensagemDesenvolvedor = ex.getCause().toString();
+		
+		//para mostrar o erro para o usuario e o desenvolvedor
+		class Erro {
+			private String mensagemUsuario;
+			private String mensagemDesenvolvedor;
+			
+			//gets
+			public String getMensagemUsuario() {
+				return mensagemUsuario;
+			}
+			public String getMensagemDesenvolvedor() {
+				return mensagemDesenvolvedor;
+			}
+			
+			//construtor
+			public Erro(String mensagemUsuario, String mensagemDesenvolvedor) {
+				super();
+				this.mensagemUsuario = mensagemUsuario;
+				this.mensagemDesenvolvedor = mensagemDesenvolvedor;
+			}
+		}
+		
+		return handleExceptionInternal(ex, new Erro(mensagemUsuario, mensagemDesenvolvedor), headers, HttpStatus.BAD_REQUEST, request);
 	}
 }
